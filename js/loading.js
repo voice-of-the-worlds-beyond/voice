e5741b8764c948bdd1aa65dbe73852941f63c14f
unknown <wty262690@gmail.com>
2026-06-16 21:23:23 +0100

    update home

--- js/loading.js
+++ js/loading.js
@@ -2,13 +2,13 @@ function endloading()
 {
     $(".maincontent").css("visibility","visible");
     var objLoading = document.getElementById("loading");
-    objLoading.style.opacity = "0";
+    //objLoading.style.opacity = "0";
     setTimeout(() => {
         if (objLoading != null)   
         {   
             objLoading.style.display = "none";   
         }   
-    }, "3000");
+    }, "2100");
 }
 
 
