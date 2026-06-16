ddb28760f253bf2076424ee6e97b23a1761b8cdc
unknown <wty262690@gmail.com>
2026-06-16 13:57:40 +0100

    initial

---
+++ js/loading.js
@@ -0,0 +1,27 @@
+function endloading()
+{
+    $(".maincontent").css("visibility","visible");
+    var objLoading = document.getElementById("loading");
+    objLoading.style.opacity = "0";
+    setTimeout(() => {
+        if (objLoading != null)   
+        {   
+            objLoading.style.display = "none";   
+        }   
+    }, "2100");
+}
+
+
+$(document).ready(function () {
+    /*
+    console.log("endloading")
+    endloading();
+    */
+})
+
+/*
+window.onload = function(){
+    console.log("endloading")
+    endloading();
+}
+*/
