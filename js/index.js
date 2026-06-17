94dbbb37003e96689c62ee58042d42fccf083182
unknown <wty262690@gmail.com>
2026-06-17 12:14:24 +0100

    add footer & introductions

--- js/index.js
+++ js/index.js
@@ -8,3 +8,23 @@ window.onload= function(){
     }, 1000);
 }
 function windowsize(){}
+
+
+const items = document.querySelectorAll('li');
+
+function update() {
+    const center = window.innerHeight / 2;
+
+    items.forEach(item => {
+        const rect = item.getBoundingClientRect();
+
+        if (rect.top < center) {
+            item.classList.add('active');
+        } else {
+            item.classList.remove('active');
+        }
+    });
+}
+
+window.addEventListener('scroll', update);
+update();
